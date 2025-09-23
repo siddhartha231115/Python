130e629e73aeb55807146b8ed3d892f270ad95b8
siddhartha231115 <drmpk86@gmail.com>
2025-09-23 17:44:15 +0530

    Add files via upload

---
+++ class2.py
@@ -0,0 +1,15 @@
+"""print("hello world")
+print(2025)
+print("my age is",10)
+print("my weight is",32)"""
+
+"""#variable 
+name="siddhartha"
+print("my name is",name)
+grade=4
+print("i am in grade",grade)
+a=("siddhartha\n kumara\n kavya ")
+print(a)"""
+
+import keyword
+print(keyword.kwlist)
