ed914cb6243dcf0c7ad9501a6fb8084d83cb7321
siddhartha231115 <drmpk86@gmail.com>
2025-09-24 18:44:45 +0530

    Add files via upload

---
+++ class3.py
@@ -0,0 +1,19 @@
+"""a = 5
+print("type of a:",type(a))
+b = 2.5
+print("type of b:",type(b))
+c = "coding"
+print("type of c:",type(c))
+d = True
+print("type of d:",type(d))
+e = float(a)
+print("type of e",type(e))
+f = str(a)
+print("type of f",type(f))
+g =  bool(a)
+print("type of g",type(g))
+h = bool(c)
+print("type of h",type(h))"""
+
+name= input("enter a word ")
+print(name[::-1])
