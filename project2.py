a3c1fd8616b4993b4600f82c5993e5c42a5b2ab9
siddhartha231115 <drmpk86@gmail.com>
2025-09-26 13:32:54 +0530

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,15 @@
+#variable 
+birthday=1
+print("my birthday is",birthday)
+#variable 
+birthday=2
+print("my birthday is",birthday)
+#variable 
+birthday=3
+print("my birthday is",birthday)
+#variable 
+birthday=4
+print("my birthday is",birthday)
+#variable 
+birthday=4
+print("my birthday is",birthday)
