5cde2fc0ffe606f96e7783a4075720e66115d1aa
siddhartha231115 <drmpk86@gmail.com>
2025-09-27 14:45:13 +0530

    Add files via upload

---
+++ class6.py
@@ -0,0 +1,28 @@
+"""height = float(input("enter your height in cm:"))
+weight = float(input("enter your weight in kg:"))
+BMI = weight / (height/100)**2
+print("your BMI is",BMI)
+if BMI <= 18.4:
+    print("you are underweight") 
+elif BMI <= 29.9:
+    print("you are healthy")
+elif BMI <= 34.9:
+    print("you are over weight")
+elif BMI <= 39.9:
+    print("you are obese")
+else:
+    print("you are severely obese")"""
+
+"""a= 10
+b = 12
+c = 0
+if a and b and c:
+    print("all the numbers have boolean value as True")
+else:
+    print("at least one number has boolean value as False")"""
+
+a = 10
+b = 12
+c = 12
+print(a != b)
+print(b != c)
