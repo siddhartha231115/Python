008c0d6f8cff5d950d5be57fcbe616fd3c9a3801
siddhartha231115 <drmpk86@gmail.com>
2025-09-29 17:50:54 +0530

    Add files via upload

---
+++ project6.py
@@ -0,0 +1,5 @@
+ch = input("please enter a character")
+if ((ch == 'a' and ch <= 'z')or (ch =='A' and 'Z')):
+    print("the given charecter ",ch,"is a english charecter")
+else:
+    print("the given charecter",ch,"is not a english charecter")
