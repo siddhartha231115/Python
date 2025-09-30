84095bfb9fa7eb9f57c049c60f2bfeae952585f5
siddhartha231115 <drmpk86@gmail.com>
2025-09-30 17:43:42 +0530

    Add files via upload

---
+++ class8.py
@@ -0,0 +1,19 @@
+"""for i in range(1,21,5):
+    print(i)"""
+
+"""n = int(input("enter the number whose sum you  want to find-"))
+sum = 0
+for i in range(1,n+1):
+    sum= sum+i
+print(sum)"""
+
+"""string = input("please enter a string:")
+string2 = ("")
+for i in string:
+    string2=i+string2
+print("\nthe orignal string =",string)
+print("the reversed string =", string2)"""
+
+n = int(input("enter the value of n:"))
+for i in range(n,0,-1):
+    print(i)
