4c4dee52d4a4901e3c999874b033d8506e06dcc9
siddhartha231115 <drmpk86@gmail.com>
2025-10-29 18:45:46 +0530

    Add files via upload

---
+++ class23.py
@@ -0,0 +1,29 @@
+"""numbers1 = [1,2,3]
+numbers2 = [4,5,6] 
+result = map(lambda x,y: x + y,numbers1,numbers2 )
+print("addition of two lists")
+print(list(result))
+nums = [1,2,3,4,5]
+def sq(n):
+    return n*n
+square = list(map(sq, nums))
+print("square of numbers in list")
+print(square)"""
+
+"""s1 = {2,3,1}
+s2 = {"b" , "a","c"}
+s3 = list(zip(s1,s2))
+print(s3,"\n")
+list1 = [10,20,30,40]
+list2 = [100,200,300,400]
+for x,y in zip(list1,list2[::-1]):
+    print(x,y)
+stocks = ["relince","infoys","tcs"]
+prices = [2175,1127,2750]
+new_dict = {stocks:prices for stocks,prices in zip(stocks,prices)}
+print("\n{}".format(new_dict))"""
+for i in range(10):
+    if i == 5:
+        print(exit)
+        exit()
+    print(i)
