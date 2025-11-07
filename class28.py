891aea5a7d940f9af514076b0ab63fb123406fa0
siddhartha231115 <drmpk86@gmail.com>
2025-11-07 18:13:45 +0530

    Add files via upload

---
+++ class28.py
@@ -0,0 +1,26 @@
+"""class myclass:
+    __privateVar = 27;
+    def __privMeth(self):
+        print("i am inside my class myclass")
+    def hello(self):
+        print("private variable value:",myclass.__privateVar) 
+foo = myclass()
+foo.hello
+foo.__privMeth"""
+
+"""class computer:
+    def __init__(self):
+        self.__maxprice=900
+    def sell(self):
+        print("selling price: {}".format(self.__maxprice))
+    def setmaxprice(self,price):
+        self.__maxprice = price
+c = computer()
+c.sell()
+c.__maxprice=1000
+c.sell()
+c.setmaxprice(1000)
+c.sell()"""
+
+class point:
+    
