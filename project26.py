e19bfa061a303d38d22536ad55493f328c591a53
siddhartha231115 <drmpk86@gmail.com>
2025-11-21 16:25:04 +0530

    Add files via upload

---
+++ project26.py
@@ -0,0 +1,8 @@
+def shutdown():
+    print("shutting down application...")
+    print("cleanup complete.")
+def main():
+    print("program running...")
+    shutdown()
+if __name__ == "__main__":
+    main()
