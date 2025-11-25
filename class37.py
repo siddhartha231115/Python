152b0312f1b9f8e6b72625a8238e31c8842e3d76
siddhartha231115 <drmpk86@gmail.com>
2025-11-25 17:44:34 +0530

    Add files via upload

---
+++ class37.py
@@ -0,0 +1,9 @@
+from tkinter import *
+window = Tk()
+window.title("demo window")
+window.geometry("400x300")
+lbl = Label(text="hey there!!!!!!!")
+lbl.pack()
+lbll = Label(text="welcomme to class37.py!!!!!!!!!!")
+lbll.pack()
+window.mainloop()
